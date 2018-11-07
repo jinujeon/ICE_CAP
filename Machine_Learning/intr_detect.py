39ca0c798ef389246ede5f3ac2b3d370c7b616eb
ddbbsoyeon <43229447+ddbbsoyeon@users.noreply.github.com>
2018-11-07 20:29:32 +0900

    Update intr_detect.py

--- Machine_Learning/intr_detect.py
+++ Machine_Learning/intr_detect.py
@@ -1,7 +1,7 @@
 def isIntrusion(colist, x, y):
     #colist = [x1, y1, x2, y2]
     #x2 > x1
-    #직선 경계선을 만드는 두 개의 좌표를 각각 0.1 * 화면의 가로 혹은 세로 만큼 빼고 더해서 두 개의 직선을 더 만든다
+    #직선 경계선을 만드는 두 개의 좌표를 각각 0.1 만큼 빼고 더해서 두 개의 직선을 더 만든다
     #객체의 꼭짓점이 두 직선 사이에 머무르는 시간이 5초 이상이 되면 경고
     warning = False
     newlist  = []
@@ -10,8 +10,6 @@ def isIntrusion(colist, x, y):
 
     for i in range(0, len(colist)):
             newlist.append(colist[i] - 0.1)
-    #newlist = [colist[0] + 128, colist[1] + 72, colist[2] + 128, colist[3] + 72,
-    #           colist[0] - 128, colist[1] - 72, colist[2] - 128, colist[3] - 72 ]
     #newlist = [0, 1, 2, 3, 4, 5, 6, 7]
     #newlist = [x11, y11, x12, y12, x21, y21, x22, y22]
     gr1 = (newlist[3] - newlist[1]) / (newlist[2] - newlist[0]) #기울기
