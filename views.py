a0b00c309cdcb273c27d4a16bcdc4df353bf12d4
Jinu <39368262+ruldy99@users.noreply.github.com>
2018-11-21 01:32:10 +0900

    Streaming
    Streaming v2

--- views.py
+++ views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 import cv2
 import numpy as np
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, request
 import threading
 from django.views.decorators.gzip import gzip_page
 import gzip
 
 class VideoCamera(object):
     def __init__(self):
+        self.k = True
         self.video = cv2.VideoCapture(0)
         (self.grabbed, self.frame) = self.video.read()
         threading.Thread(target=self.update, args=()).start()
@@ -24,17 +25,32 @@ class VideoCamera(object):
         while True:
             (self.grabbed, self.frame) = self.video.read()
 
-cam = VideoCamera()
+cam = VideoCamera() #서버 실행시 최초 1회만 실행
 
 def gen(camera):
+    # cam = VideoCamera()
+    if cam.k == False:
+        cam.__init__()
     while True:
         frame = cam.get_frame()
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 #@gzip.gzip_page
 def livefe(request):
     try:
         return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
-        pass
+    except ConnectionAbortedError as e:
+        print(e)
+
+def homeview(request):
+    return render(request,'blog/index.html')
+
+def request_page(request):
+    cam.__del__()
+    cam.k = False
+    return render(request, 'blog/indextest.html')
+
+
+
