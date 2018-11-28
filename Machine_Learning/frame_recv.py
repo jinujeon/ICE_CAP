067ca360bd434facc47a96c1bd443ca8949d71c5
glad1470 <glad1470@gmail.com>
2018-11-29 04:26:17 +0900

    Update frame_recv.py

--- Machine_Learning/frame_recv.py
+++ Machine_Learning/frame_recv.py
@@ -366,12 +366,12 @@ cam = Cam()
 MODEL_NAME = 'inference_graph'
 CWD_PATH = "C:/models/research/object_detection"
 # CWD_PATH = 'C:/tensorflow1/models/research/object_detection'
-PATH_TO_CKPT = os.path.join(self.CWD_PATH, self.MODEL_NAME, 'frozen_inference_graph.pb')
-PATH_TO_LABELS = os.path.join(self.CWD_PATH, 'training', 'labelmap.pbtxt')
+PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
+PATH_TO_LABELS = os.path.join(CWD_PATH, 'training', 'labelmap.pbtxt')
 NUM_CLASSES = 5
-label_map = label_map_util.load_labelmap(self.PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(self.label_map, max_num_classes=self.NUM_CLASSES,use_display_name=True)
-category_index = label_map_util.create_category_index(self.categories)
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
 detection_graph = tf.Graph()
 with detection_graph.as_default():
     od_graph_def = tf.GraphDef()
@@ -474,7 +474,7 @@ while True:
         np.squeeze(boxes),
         np.squeeze(classes).astype(np.int32),
         np.squeeze(scores),
-        cam.category_index,
+        category_index,
         cam,
         use_normalized_coordinates=True,
         min_score_thresh=0.60)
