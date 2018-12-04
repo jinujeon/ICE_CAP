495b36b297093f47dae634ec331d2e99c5dd7dac
Chockchockhancookie <mellow3632@naver.com>
2018-12-05 03:00:24 +0900

    sms(complete)

--- Django_channels/mysite/notifier/urls.py
+++ Django_channels/mysite/notifier/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import url
 from . import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     #url(r'^$', views.change_stat),
     path("home/change_stat", views.change_stat),
     path('monitor/home/alert.html',views.AlertView.as_view(), name = 'alert'),
     #path('audios/note.mp3',views.AlertView),
     path('edum/',auth_views.LoginView.as_view(), name='login'),
-    path('monitor/sms',views.run, name="run"),
+   # path('monitor/sms',views.run, name='run'),
     url(r'streamtest/', views.livefe, name='livefe'),
-
+    url(r'loginteste/', views.profile, name='profile'),
 ]
--- Django_channels/mysite/test_send.py
+++ Django_channels/mysite/test_send.py
@@ -3,7 +3,7 @@ import urllib.request
 
 url = "http://127.0.0.1:8000/home/change_stat"  # URL
 
-d = {'cam_id': 0, 'cam_status': 'warning', 'cam_location': 'Engineering_Univ.1st.right_corridor', 'trash': False, 'instrusion': False, 'fallen': False}
+d = {'cam_id': 0, 'cam_status': 'warning', 'cam_location': '공학관1층 복도', 'trash': False, 'instrusion': False, 'fallen': False}
 params = json.dumps(d).encode("utf-8")  
 req = urllib.request.Request(url, data=params,
                              headers={'content-type': 'application/json'})
