962ad63139eb624e7722a2a3c2686d72541f37fa
glad1470 <glad1470@gmail.com>
2018-12-05 06:18:05 +0900

    Update frame_recv.py

--- Machine_Learning/frame_recv.py
+++ Machine_Learning/frame_recv.py
@@ -103,7 +103,7 @@ class Cam(threading.Thread):
         threading.Thread.__init__(self, name='Cam({})'.format(id))
         self.id = None
         self.frame = None
-        self.data = {'cam_id': self.id, 'cam_status': 'safe', 'trash': False, 'intrusion': False, 'fallen': False,'restricted':False}
+        self.data = {'cam_id': self.id, 'trash': False, 'intrusion': False, 'fallen': False,'restricted':False,'fence':False}
         self.url = "http://127.0.0.1:8000/home/change_stat"
         self.actrec = actRecognition()
         self.e_list = []
@@ -136,7 +136,6 @@ class actRecognition():
         self.fence_warning = False  # 월담 감지
         self.fence_prev = []  # 월담 구역에서 사람 경계 박스의 이전 위치 저장 list
         self.t_prev = []  # 쓰레기 투기 감지 시 쓰레기와 사람 경계 박스의 이전 위치 저장 list
-        self.IntrFirst = True  # 월담 감지 초기화 구분
         self.trFirst = True  # 쓰레기 감지 초기화 구분
         self.peopleNum = 0  # 월담 감지에서의 사람 수
         self.pID = 0  # 쓰레기 감지에서의 쓰레기를 들고 있는 사람 좌표가 저장된 인덱스
@@ -144,57 +143,80 @@ class actRecognition():
         self.midTr = []  # 쓰레기의 중점 좌표
         self.midP = []  # 쓰레기를 들고 있는 사람의 중점 좌표
         self.trashMulti = cv2.MultiTracker_create()  # 쓰레기 감지 추적기
-        self.colist = [10, 330, 630, 330]  # 가상 펜스 직선을 그릴 좌표
+        self.colist1 = [10, 280, 630, 280]  # 가상 펜스 직선을 그릴 좌표 - 위 직선
+        self.colist2 = [10, 330, 630, 330]  # 가상 펜스 직선을 그릴 좌표 - 아래 직선
         self.trash_time = 0
         self.trashnum = 0
-        self.fence_tracker = cv2.MultiTracker_create()  # 월담 감지 추적기
+        self.fence_tracker = cv2.MultiTracker_create() # 월담 감지 추적기
 
-    def IntrSettings(self, cam):
+    def fence_settings(self, cam):
         '''
         월담 감지에서의 객체 추적기를 초기화하여 사람 좌표를 추가해 세팅함
         :param cam:
         :return:
         '''
-        self.intr_prev = []
-        self.id = 0
-        self.peopleNum = len(cam.fxy_list)
+        self.fence_prev = []
+        self.fence_tracker = cv2.MultiTracker_create()
         for i in cam.fxy_list:
-            i0, i2 = abs(int(i[0] * 640)), abs(int(i[2] * 640))
-            i1, i3 = abs(int(i[1] * 480)), abs(int(i[3] * 480))
-            window = (i0, i1, i2, i3)
-            # print(window)
-            self.intr_prev.append(window)
+            x, w = abs(int(i[0] * 640)), abs(int(i[2] * 640))
+            y, h = abs(int(i[1] * 480)), abs(int(i[3] * 480))
+            cv2.rectangle(cam.frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            window = (x, y, w, h)
             try:
                 csrt = cv2.TrackerCSRT_create()
-                self.intrMulti.add(csrt, cam.frame, window)
+                self.fence_tracker.add(csrt, cam.frame, window)
             except cv2.error:
                 pass
+            if (y + h) > self.colist1[1] and y > self.colist1[1]:  # 담 너머의 사람일 때 마지막 원소 값 = 0
+                self.fence_prev.append([x, y, w, h, 0])
+            # 담 앞의 사람이고 상체만 인식되었을 때 마지막 원소 값 = 1
+            elif (y + h) > self.colist1[1] and y < self.colist1[1] and y > self.colist2[1]:
+                self.fence_prev.append([x, y, w, h, 1])
+            # 담 앞의 사람이고 상하체가 모두 인식되었을 때 마지막 원소 값 = 2
+            else:
+                self.fence_prev.append([x, y, w, h, 2])
+        self.peopleNum = len(self.fence_prev)
+        print("init = ", self.fence_prev)
 
-    def IntrUpdates(self, cam):
+    def fence_updates(self, cam):
         '''
-        IntrSettings에서 세팅된 객체 추적기를 업데이트하여 같은 사람에 대해 상자 좌표의 수직을 비교,
-        30 픽셀 이상 위로 움직였을 때 월담을 감지하여 DB 업데이트
+
         :param cam:
         :return:
         '''
-        self.id = 0
-        try:
-            (success, boxes) = self.intrMulti.update(cam.frame)
-            temp = []
-            for box in boxes:
-                [x, y, w, h] = [abs(int(v)) for v in box]
-                temp.append([x, y, w, h])
-                if self.id < len(self.intr_prev):
-                    # print("차이", self.intr_prev[self.id][1] - y)
-                    if self.intr_prev[self.id][1] - y > 30:  # y축 변화가 위로 수직일 때만
-                        print("******침입 감지 ! ! ! ! !*******")
-                        self.intr_warning = True
-                self.id += 1
-                cv2.rectangle(cam.frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            self.intr_prev = temp
-            # print("prev= ", self.intr_prev)
-        except cv2.error:
-            pass
+        (success, boxes) = self.fence_tracker.update(cam.frame)
+        print("update success:", success)
+        temp = []
+        for box in boxes:
+            [x, y, w, h] = [abs(int(v)) for v in box]
+            cv2.rectangle(cam.frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            if (y + h) > self.colist1[1] and y > self.colist1[1]:  # 담 너머의 사람일 때 마지막 원소 값 = 0
+                temp.append([x, y, w, h, 0])
+            # 담 앞의 사람이고 상체만 인식되었을 때 마지막 원소 값 = 1
+            elif (y + h) > self.colist1[1] and y < self.colist1[1] and y > self.colist2[1]:
+                temp.append([x, y, w, h, 1])
+            # 담 앞의 사람이고 상하체가 모두 인식되었을 때 마지막 원소 값 = 2
+            else:
+                temp.append([x, y, w, h, 2])
+
+        for index, i in enumerate(temp):
+            if index < len(self.fence_prev):
+                # 담 뒤에 있던 사람이 앞으로 간 것으로 위치가 바뀐 경우 월담 감지
+                if self.fence_prev[index][4] == 0 and y < self.colist1[1]:
+                    self.fence_warning = True
+                    print("뒤 -> 앞 월담 감지")
+                    i[4] = 2
+                # 담 앞에 있던 사람이 일정 픽셀 올라감과 동시에 뒤로 간 것으로 위치가 바뀐 경우 월담 감지
+                elif self.fence_prev[index][4] == 1 and y > self.colist2[1]:
+                    self.fence_warning = True
+                    print("앞 -> 뒤 월담 감지 (몸 전체 인식)")
+                    i[4] = 0
+                elif self.fence_prev[index][4] == 2 and y > self.colist1[1]:
+                    self.fence_warning = True
+                    print("앞 -> 뒤 월담 감지 (상체부분 인식)")
+                    i[4] = 0
+        self.fence_prev = temp
+        print("now = ", self.fence_prev)
 
     def trSettings(self, cam):
         '''
@@ -209,33 +231,27 @@ class actRecognition():
         window = ()
         i0, i1, i2, i3, self.trDistance = 0, 0, 0, 0, 0
         self.midTr, self.midP = [], []
-        for i in cam.txy_list:  # 쓰레기 경계 박스 좌표를 tensorflow 기준에서 opencv 기준으로
+        list = cam.txy_list + cam.fxy_list
+        self.trashnum = len(cam.txy_list)
+        for i in list:
             i0, i2 = abs(int(i[0] * 640)), abs(int(i[2] * 640))
             i1, i3 = abs(int(i[1] * 480)), abs(int(i[3] * 480))
             window = (i0, i1, i2, i3)
+            if i >= self.trashnum and ((i0 <= self.t_prev[0][0] <= (i0 + i2)) or
+                                       (i0 <= (self.t_prev[0][2] + self.t_prev[0][0]) <= (i0 + i2))):
+                    if len(self.t_prev) > 1:
+                        self.pID = len(self.t_prev) - 1  # 쓰레기를 들고 있는 사람
+                    self.midTr = [(self.t_prev[0][0] * 2 + self.t_prev[0][2]) / 2,
+                                  (self.t_prev[0][1] * 2 + self.t_prev[0][3]) / 2]
+                    self.midP = [(i0 * 2 + i2) / 2, (i1 * 2 + i3) / 2]
+                    # 쓰레기와 사람 경계 박스의 중점 간 거리를 계산
+                    self.trDistance = math.sqrt(
+                        math.pow(self.midTr[0] - self.midP[0], 2) + math.pow(self.midTr[1] - self.midP[1], 2))
+                    print("처음 거리:", self.trDistance)
             self.t_prev.append(window)
             try:
                 self.trashMulti.add(csrt, cam.frame, window)  # 객체 추적기에 쓰레기 좌표 추가
-            except cv2.error:
-                pass
-        for i in cam.fxy_list:  # 사람 경계 박스 좌표를 tensorflow 기준에서 opencv 기준으로
-            i0, i2 = abs(int(i[0] * 640)), abs(int(i[2] * 640))
-            i1, i3 = abs(int(i[1] * 480)), abs(int(i[3] * 480))
-            window = (i0, i1, i2, i3)
-            self.t_prev.append(window)
-            # 사람 경계 박스를 계산 중 쓰레기 경계 박스의 가로와 사람 경계 박스의 가로가 겹치면
-            if (i0 <= self.t_prev[0][0] <= (i0 + i2)) or (i0 <= (self.t_prev[0][2] + self.t_prev[0][0]) <= (i0 + i2)):
-                if len(self.t_prev) > 1:
-                    self.pID = len(self.t_prev) - 1  # 쓰레기를 들고 있는 사람
-                self.midTr = [(self.t_prev[0][0] * 2 + self.t_prev[0][2]) / 2, (self.t_prev[0][1] * 2 + self.t_prev[0][3]) / 2]
-                self.midP = [(i0 * 2 + i2) / 2, (i1 * 2 + i3) / 2]
-                # 쓰레기와 사람 경계 박스의 중점 간 거리를 계산
-                self.trDistance = math.sqrt(
-                    math.pow(self.midTr[0] - self.midP[0], 2) + math.pow(self.midTr[1] - self.midP[1], 2))
-                # print("처음 거리:", self.trDistance)
-            try:
-                # print("사람 좌표 추가")
-                self.trashMulti.add(csrt, cam.frame, window)  # 객체 추적기에 사람 좌표 추가
+                cv2.rectangle(cam.frame, (i0, i1), (i0 + i2, i1 + i3), (255, 0, 0), 2)
             except cv2.error:
                 pass
 
@@ -255,14 +271,12 @@ class actRecognition():
 
                 cv2.rectangle(cam.frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
             if len(temp) == 0:
-                # print("추적기에 아무것도 없음")
-                pass
+                print("추적기에 아무것도 없음")
             else:
-                # print(temp)
-                pass
+                print(temp)
             self.midTr = [(temp[0][0] * 2 + temp[0][2]) / 2, (temp[0][1] * 2 + temp[0][3]) / 2]
             if self.pID < len(temp):
-                # print("중점 계산")
+                print("중점 계산")
                 self.midP = [(temp[self.pID][0] * 2 + temp[self.pID][2]) / 2,
                              (temp[self.pID][1] * 2 + temp[self.pID][3]) / 2]
             # 쓰레기를 들고 있던 사람 경계 박스의 중점과 쓰레기 경계 박스 중점 간 거리를 계산
@@ -275,7 +289,7 @@ class actRecognition():
         except cv2.error:
             pass
 
-    def fence_compute(self, x, y):
+    def intr_compute(self, x, y):
         '''
         가상 펜스 경계 직선과 한 꼭짓점 간의 위치 파악 후 한 꼭짓점에 대한 boolean return
         :param x: 사람 객체 상자 한 꼭짓점의 x 좌표
@@ -297,12 +311,12 @@ class actRecognition():
         :return:
         '''
         if ((('trash' in cam.e_list) or ('metal' in cam.e_list) or (
-                'bottle' in cam.e_list))):  # 쓰레기가 처음 감지되었을 때
+                'bottle' in cam.e_list)) or self.trFirst):  # 쓰레기가 처음 감지되었을 때
             self.trash_time = 0
             self.trSettings(cam)
             self.trFirst = False
         elif (('trash' in cam.e_list) or ('metal' in cam.e_list) or ('bottle' in cam.e_list)) and (
-                'person' in cam.e_list):  # 쓰레기와 사람이 모두 감지되고 있을 때
+                'person' in cam.e_list) and not self.trFirst:  # 쓰레기와 사람이 모두 감지되고 있을 때
             self.trash_time = 0
             self.trUpdates(cam)
         elif ('trash' in cam.e_list) or ('metal' in cam.e_list) or ('bottle' in cam.e_list):
@@ -316,24 +330,22 @@ class actRecognition():
                 self.trash_time = time.time()
             if time.time() - self.trash_time >= 10:
                 self.trash_time = 0
-                #self.trFirst = True
+                self.trFirst = True
 
         # 상황 판단 DB 업데이트
         if self.trash_warning:
+            print("쓰레기 무단 투기가 감지 되었습니다.")
             if cam.data['trash'] == False:
-                cam.data['cam_status'] = 'warning'
                 cam.data['trash'] = True
-                print("쓰레기 무단 투기가 감지 되었습니다.")
             self.trash_warning = False
         else:
             if cam.data['trash'] == True:
-                cam.data['cam_status'] = 'safe'
                 cam.data['trash'] = False
 
-    def fence_check(self, cam):
+    def intr_check(self, cam):
         '''
         가상 펜스 경계를 넘었는지 확인하는 함수
-        fence_compute 를 통해 각 꼭짓점의 침입 여부 계산
+        intr_compute 를 통해 각 꼭짓점의 침입 여부 계산
         :param cam:
         :return:
         '''
@@ -342,40 +354,42 @@ class actRecognition():
             stat = []
             for m in range(0, len(cam.fxy_list[i]), 2):
                 # 네 개의 꼭짓점 하나씩을 가상펜스 직선과 비교하여 그 위에 위치해있으면 True, 아니면 False
-                stat.append(self.fence_compute(cam.fxy_list[i][m], cam.fxy_list[i][m + 1]))
+                stat.append(self.intr_compute(cam.fxy_list[i][m], cam.fxy_list[i][m + 1]))
             for k in stat:
                 # 네 개의 꼭짓점이 모두 가상 펜스 위에 위치해있으면 최종적으로 f_stat에 True가 저장
                 f_stat = f_stat and k
-            self.fence_warning = f_stat
-        if self.fence_warning:
+            self.intr_warning = f_stat
+        if self.intr_warning:
+            print("접근 제한 구역 침입이 감지되었습니다")
             if cam.data['intrusion'] == False:
                 cam.data['cam_status'] = 'warning'
                 cam.data['intrusion'] = True
-                print("접근 제한 구역 침입이 감지되었습니다")
-            self.fence_warning = False
+
+            self.intr_warning = False
         else:
             if cam.data['intrusion'] == True:
-                cam.data['cam_status'] = 'safe'
-                cam.data['intrusion'] = False
                 print("해당 구역은 안전합니다(가상 펜스)")
+                cam.data['intrusion'] = False
 
     def fallen_check(self,cam):
         '''
-        쓰러진 사람 인식 시 바로 DB 업데이트
+        쓰러진 사람 인식 시 DB 업데이트
         :return:
         '''
         if 'fallen' in cam.e_list:
-            if cam.data['fallen'] == False:
-                cam.data['cam_status'] = 'warning'
-                cam.data['fallen'] = True
-                print("쓰러진 사람 발견")
+            if self.fallen_time == 0:
+                self.fallen_time = time.time()
+            if time.time() - self.fallen_time >= 10:
+                if cam.data['fallen'] == False:
+                    cam.data['fallen'] = True
+                    print("쓰러진 사람 발견")
         else:
+            self.fallen_time = 0
             if cam.data['fallen'] == True:
-                cam.data['cam_status'] = 'safe'
                 cam.data['fallen'] = False
                 print("쓰러진 사람 없음")
 
-    def Intrusion_detect(self, cam):
+    def fence_detect(self, cam):
         '''
         월담 감지 진행
         :param cam:
@@ -383,27 +397,21 @@ class actRecognition():
         '''
         # 사람 수가 0이 아니고 사람 수가 변경되었을 때
         if len(cam.fxy_list) != 0 and self.peopleNum != len(cam.fxy_list):
-            if not self.IntrFirst:  # 사람이 처음, 혹은 인식되지 않다가 다시 인식되었다면
-                self.intrMulti = cv2.MultiTracker_create()
-            else:
-                self.IntrFirst = False
-            self.IntrSettings(cam)  # 객체 추적기 초기화
+            self.fence_settings(cam)  # 객체 추적기 초기화
         elif len(cam.fxy_list) != 0:  # 사람 수의 변경이 없고 사람이 있을 때
-            self.IntrUpdates(cam)  # 객체 추적기 update
+            self.fence_updates(cam)  # 객체 추적기 update
         else:  # 사람이 없을 때
-            self.IntrFirst = True  # 첫 인식 변수 초기화
+            pass
 
-        if self.intr_warning:
-            if cam.data['intrusion'] == False:
-                cam.data['cam_status'] = 'warning'
-                cam.data['intrusion'] = True
+        if self.fence_warning:
+            if cam.data['fence'] == False:
                 print("월담을 감지했습니다")
-            self.intr_warning = False
+                cam.data['fence'] = True
+            self.fence_warning = False
         else:
-            if cam.data['intrusion'] == True:
-                cam.data['cam_status'] = 'safe'
-                cam.data['intrusion'] = False
+            if cam.data['fence'] == True:
                 print("해당 구역은 안전합니다(월담)")
+                cam.data['fence'] = False
 
     def send_post(self,cam):
         params = json.dumps(cam.data).encode("utf-8")
@@ -414,15 +422,16 @@ class actRecognition():
 
     def draw_line(self,cam):
         if cam.data['restricted']:
-            cv2.line(cam.frame, (self.colist[0], self.colist[1]), (self.colist[2],self.colist[3]),(255,0,0),2)
+            cv2.line(cam.frame, (self.colist1[0], self.colist1[1]), (self.colist1[2],self.colist1[3]),(255,0,0),2)
+            cv2.line(cam.frame, (self.colist2[0], self.colist2[1]), (self.colist2[2],self.colist2[3]),(255,0,0),2)
 
     def run(self,cam):
         if cam.data["restricted"]:
-            cam.actrec.Intrusion_detect(cam)
-        cam.actrec.fence_check(cam)
+            cam.actrec.intr_check(cam)
+        cam.actrec.fence_detect(cam)
         cam.actrec.fallen_check(cam)
         cam.actrec.Trash_detect(cam)
-        cam.actrec.send_post(cam)
+        # cam.actrec.send_post(cam)
 
 class Obj_detection():
     def __init__(self,sess,detection_boxes, detection_scores, detection_classes, num_detections,image_tensor,frame_expanded,cam):
