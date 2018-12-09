adb14906db4d02f52d97dbfdd77a044634d79724
jy016011 <jy016011@naver.com>
2018-12-10 00:40:53 +0900

    Now, filter gets feedback from detection module

--- Django_channels/Filter.py
+++ Django_channels/Filter.py
@@ -60,17 +60,19 @@ class Frame_sender:
         ##입력받은 카메라 수 만큼 video_capture object를 생성##
         for id in id_list:
             exec('self.video_capture_{} =  VideoCamera({})'.format(id,id))
-            exec('self.video_list.append(self.video_capture_{})'.format(id)) #exec로 생성한 videocapture객체에 접근하기 위해 리스트에 저장
+            exec('self.video_list.append(self.video_capture_{})'.format(id))
             self.video_list[id].write(id)
         self.cam_count = len(id_list)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((address, port))
         self.encode_param = encode_param
         self.max_fps = 10  # maximum frame per second
-        self.scheduler = Scheduler.Frame_scheduler(self.id_list,self.max_fps,self.socket)
+        self.scheduler = Scheduler.Frame_scheduler(self.id_list,self.max_fps)
         self.last_change_schedule_time = time.time()
         self.index = [0, 0]
         self.wall = wall
+        self.cam_weight_dict = dict([(id, 1) for id in range(self.cam_count)])
+
     def initialize_server(self):
         while True:
             try:
@@ -86,11 +88,14 @@ class Frame_sender:
                 logging.error('TimeoutError')
 
     def send_info(self,id, virtual, wall,sock):
+        '''프레임 전송 전, 연결되어있는 카메라의 정보들을 전송'''
         req = "cam_id:" + str(id) + "\r\nrestricted:" + str(virtual) + "\r\nwall:" +str(wall)
         sock.send(req.encode('UTF-8'))  # send message to server
         return print("Send Complete")
 
     def send_frame(self,cam_id,idx,schedule):
+        '''카메라로부터 프레임을 읽어오고, 저장하며,
+            프레임을 전송한다.'''
         self.video_list[cam_id].sizecon()
         if self.video_list[cam_id].sizecontrol % 1 == 0:
             self.video_list[cam_id].storeframe(self.video_list[cam_id].frame)
@@ -104,7 +109,7 @@ class Frame_sender:
             print("##SIZE{}##: ".format(cam_id), size)
             print("###INDEX",idx)
             try:
-                if schedule[idx] == cam_id:
+                if schedule[idx] == cam_id:# 스케줄링된 순서에 맞는 카메라의 프레임을 전송
                     self.socket.send(str(cam_id).encode('UTF-8'))
                     data = self.socket.recv(1024)
                     print("Echo cam num:", data)
@@ -114,19 +119,19 @@ class Frame_sender:
                 logging.error('ConnectionResetError')
             except ConnectionAbortedError:
                 logging.error('ConnectionAbortedError')
-            cv2.imshow('Cam {}'.format(cam_id), self.frame_list[cam_id])
+            cv2.imshow('Cam {}'.format(cam_id), self.frame_list[cam_id])#카메라로부터 읽어들여온 프레임들을 보여준다.
 
 
     def run(self):
         self.initialize_server()
         idx = 0
-        self.scheduler.set_cam_frame_order()
+        self.scheduler.set_cam_frame_order()#스케줄 순서 default 값
         while True:
             # Capture frame-by-frame
+            id = 0
             schedule = self.scheduler.schedule
             print(schedule)
-            id = 0
-            if idx >= len(schedule):
+            if idx >= len(schedule):#스케줄을 넘어갈시 다시 스케줄의 처음부터 카메라의 순서에 맞게 프레임 전송
                 idx = 0
             for video in self.video_list:
                 exec('self.ret{}, self.frame{} = video.read()'.format(id,id))
@@ -137,18 +142,19 @@ class Frame_sender:
             for cam_id in range(len(self.ret_list)):
                 self.capture(cam_id)
                 self.send_frame(cam_id, idx,schedule)
+                #print("send complete")
+            self.recv_detect_info(schedule[idx])#가중치 피드백 받기
+            self.scheduler.set_cam_frame_order(1, self.cam_weight_dict)#받은 가중치로 스케줄을 다시 조정
             self.frame_list = []
             self.ret_list = []
             idx += 1
-            if ((time.time() - self.last_change_schedule_time) > 5):
-                self.scheduler.set_cam_frame_order(1)
-                self.last_change_schedule_time = time.time()
-            #self.recv_detect_info()
 
 
-    def recv_detect_info(self):
+    def recv_detect_info(self,cam_id):
         data = self.socket.recv(1024)
+        #print("Receive_complete")
         self.detect_info = data.decode('UTF-8')
+        self.cam_weight_dict[cam_id] = int(self.detect_info)
 
     def capture(self, camid):
         # png로 압축 없이 영상 저장
@@ -163,13 +169,13 @@ class Frame_sender:
 
 
 def main():
-    id_list = [0] #설치되어 있는 카메라 id
+    id_list = [0,1] #설치되어 있는 카메라 id
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-    # ADDRESS = '220.67.124.193'
-    ADDRESS = 'localhost'
+    ADDRESS = '220.67.124.193'
+    # ADDRESS = 'localhost'
     PORT = 8485
     # client_socket = initialize_server(ADDRESS,PORT,id_list)
-    fs1 = Frame_sender(id_list, [False],[False],ADDRESS,PORT, encode_param)
+    fs1 = Frame_sender(id_list, [False,False],[False,True],ADDRESS,PORT, encode_param)
     fs1.run()
 
 if __name__ == "__main__":
